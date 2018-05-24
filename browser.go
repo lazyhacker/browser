f1545f29851ccd1f2e11cc2c1d7e57ec55ad560c
Lazyhacker <github@lazyhacker.com>
2018-05-23 23:39:46 -0700

    first commit

---
+++ browser.go
@@ -0,0 +1,23 @@
+package browser
+
+import (
+	"syscall/js"
+)
+
+var global = js.Global
+
+type window struct {
+	Console debug
+}
+
+// GetWindow returns the main browser window object.
+func Window() window {
+	return window {
+		Console: debug{console: global.Get("console")}
+	}
+}
+
+// Alert shows an alert box in the browser with an OK button.
+func (window) Alert(m string) {
+	global.Call("alert", m)
+}
